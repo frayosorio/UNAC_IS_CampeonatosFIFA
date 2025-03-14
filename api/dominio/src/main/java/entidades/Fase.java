72cbfcc256ca7de2eb65f29ae19ef1f13b37ded3
fray osorio <frayosorio@gmail.com>
2025-03-13 20:02:34 -0500

    Se actualiza entidad FASE

--- api/dominio/src/main/java/entidades/Fase.java
+++ api/dominio/src/main/java/entidades/Fase.java
@@ -1,5 +1,42 @@
-package entidades;
+package campeonatosfifa.api.dominio.entidades;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "fase")
 public class Fase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "secuencia_fase")
+    @GenericGenerator(name = "secuencia_fase", strategy = "increment")
+    @Column(name = "id")
+    private int id;
 
+    @Column(name = "fase", length = 50, unique = true)
+    private String nombre;
+
+    public Fase() {
+    }
+
+    public Fase(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
