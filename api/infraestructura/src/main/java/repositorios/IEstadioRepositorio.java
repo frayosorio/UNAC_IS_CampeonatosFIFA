05adf537b45f9cc8f3ca8c0ebb5ef94ccff480c4
fray osorio <frayosorio@gmail.com>
2025-03-13 21:08:51 -0500

    Se agrega la capa de REPOSITORIOS

---
+++ api/infraestructura/src/main/java/repositorios/IEstadioRepositorio.java
@@ -0,0 +1,10 @@
+package campeonatosfifa.api.infraestructura.repositorios;
+
+import campeonatosfifa.api.dominio.entidades.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IEstadioRepositorio extends JpaRepository<Estadio, Integer>{
+
+}
